9ddecb82a8354ca5670787e8218dfa02e372cb8f
KYLIE REED <KYLIEEREED@GMAIL.COM>
2014-02-26 20:32:33 -0800

    Completed Animals Problem

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,13 +1,25 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
-// WRITE CODE HERE
+// 
+$('#toggle_img').click(function() {
+if($('#toggle_img').text() === "Go Away!"){
+	$('#toggle_img').text("Bring it back!")
+	$('#main_img').hide()
+} else {
+	$('#toggle_img').text("Go Away!")
+	$('#main_img').show()
+}});
 
 // Problem 3 (Swap Em) -----------------------------------------------
 // WRITE CODE HERE
+$('#change_img').click(function() {
+	$('#main_img').attr('src','/static/img/' + $('#new_img_file').val())	
+});
 
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+	$(this).attr('width', '200px');
+	$('#main_img').attr('src',$(this).attr('src'));
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
-// WRITE CODE HERE
+
