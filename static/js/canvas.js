89edb14a2bb3b81c86c082785ff75c82880d11be
KYLIE REED <KYLIEEREED@GMAIL.COM>
2014-03-05 19:53:12 -0800

    Mastered fills and colors!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -38,4 +38,37 @@ $(document).ready(function() {
   	context.stroke()
   });
 
+  $('#p6').click(function(){
+  	context.strokeStyle='green';
+  	context.strokeRect(50, 50, 100, 150)
+  });
+
+  $('#p7').click(function(){
+  	context.beginPath()
+  	context.arc(100, 100, 50, 0, 2*Math.PI)
+  	context.fillStyle='red'
+  	context.fill()
+  });
+
+  $('#p8').click(function(){
+  	context.fillStyle="yellow";
+  	context.fillRect(50, 50, 150, 150)
+  	context.strokeStyle='blue';
+  	context.strokeRect(50, 50, 150, 150)
+  });
+
+  $('#p9').click(function(){
+  	for(var i = 0; i < 251; i = i + 50){
+  		context.strokeRect(i, 100, 50, 50)
+  	}
+  });
+
+  $('#p10').click(function(){
+  	for(var i = 0; i < 501; i = i + 5)
+  		context.strokeRect(i, 50, 5, 5)
+  });
+
+  $('#p11').click(function(){
+  	for
+  })
 });
