180758a269a8b28c05c8a7f6831fffbf7187e3bd
KYLIE REED <KYLIEEREED@GMAIL.COM>
2014-03-05 19:12:00 -0800

    Mastered right angles!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -11,7 +11,31 @@ $(document).ready(function() {
   //Write your code for p1-p12 here
   //
   $('#p1').click(function(){
-  	context.strokeRect(4, 4, 7, 10)
+  	context.strokeRect(4, 4, 70, 100)
+  });
+
+  $('#p2').click(function(){
+  	context.strokeRect(4, 4 ,70 ,70 )
+  });
+
+  $('#p3').click(function(){
+  	context.beginPath()
+  	context.arc(100, 100, 50, 0, Math.PI)
+  	context.stroke()
+  });
+
+  $('#p4').click(function(){
+  	context.beginPath()
+  	context.arc(100, 100, 50, 0, 2*Math.PI)
+  	context.stroke()
+  });
+
+  $('#p5').click(function(){
+  	context.beginPath()
+  	context.moveTo(100, 100)
+  	context.lineTo(350, 350)
+  	context.closePath()
+  	context.stroke()
   });
 
 });
