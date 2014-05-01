6d39acea1f10c0a893148109c6343a70c90d1b18
KYLIE REED <KYLIEEREED@GMAIL.COM>
2014-04-30 19:40:17 -0700

    Finished lab 7!

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -19,22 +19,44 @@ $(document).ready(function() {
       context.fill();
     };
   // PUT STUFF HERE
-var b0 = {
-  radius: 20, x: 20, y: 20, vx: 5, vy: 5, color: 'red'
+var numBalls = 50;
+
+var balls = [];
+
+var colors = ['aqua', 'grey', 'magenta', 'violet', 'lightblue', 'lightcoral', 'turquoise'];
+
+for (var i = 0; i < numBalls; i++){
+    var b0 = {
+    radius: Math.random()*10+5, x: Math.random()*770+15, y: Math.random()*570+15, 
+    vx: Math.random()*7-3.5, vy: Math.random()*7-3.5, 
+    color: colors[Math.floor(Math.random()*colors.length)]
 };
-var b1 = {
-  radius: 15, x: 50, y: 80, vx: 5, vy: 5, color: 'blue'
-};
-var b2 = {
-  radius: 30, x: 80, y: 200, vx: 5, vy: 5, color: 'green'
-};
-var balls = [b0, b1, b2];
+  balls.push(b0)
+  };
 
   // Run an interation of the game
   var updateGame = function() {
+    drawSquare(0, 0, 800, 'lavender');
     for (var i = 0; i < balls.length; i++){
+      balls[i].x = balls[i].x + balls[i].vx;
+      balls[i].y = balls[i].y + balls[i].vy;
+      if (balls[i].x >= canvas.width - balls[i].radius) {
+      balls[i].vx = -balls[i].vx
+    };
+      if (balls[i].y >= canvas.height - balls[i].radius) {
+      balls[i].vy = -balls[i].vy
+    };
+      if (balls[i].x <= 0 + balls[i].radius) {
+      balls[i].vx = -balls[i].vx
+    };
+      if (balls[i].y <= 0 + balls[i].radius) {
+      balls[i].vy = -balls[i].vy
+    };
+    }; 
+  for (var i = 0; i < balls.length; i++){
       drawCircle(balls[i].x, balls[i].y, balls[i].radius, balls[i].color)
-    };       
+    };
+          
     setTimeout(updateGame, 10);
     };
 
@@ -44,6 +66,12 @@ var balls = [b0, b1, b2];
     var x = e.pageX - $(this).offset().left;
     var y = e.pageY - $(this).offset().top;
     // PUT STUFF HERE
+    var b0 = {
+    radius: Math.random()*10+5, x: x, y: y, 
+    vx: Math.random()*7-3.5, vy: Math.random()*7-3.5, 
+    color: colors[Math.floor(Math.random()*colors.length)]
+};
+  balls.push(b0)
   });
 
   updateGame();
